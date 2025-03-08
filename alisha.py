0a0aa621e7bee9ed9ad3ad08562e6d210311fe89
Alisha Muhammad Irfan <160208713+Alisha000111@users.noreply.github.com>
2025-03-08 22:09:57 +0500

    Add files via upload

---
+++ alisha.py
@@ -0,0 +1,52 @@
+# Arithmetic Operators
+a = 40
+b = 2
+print(a + b)  
+print(a - b)  
+print(a * b)  
+print(a / b)  
+print(a % b)  
+print(a // b)
+# Assignment Operators
+x = 10
+x += 1 
+x -= 3
+x *= 20 
+x /= 39 
+x %= 86 
+x **= 37
+x //= 90
+# Comparison Operators
+a = 29
+b = 79
+print(a == b) 
+print(a != b) 
+print(a > b)   
+print(a < b)   
+print(a >= b)  
+# Logical Operators
+x = True
+y = False
+print(x and y)  
+print(x or y)  
+print(not x)   
+# Identity Operators
+a = [14, 28, 32]
+b = [18, 22, 38]
+c = a
+print(a is b)     
+print(a is c)    
+print(a is not b)  
+# Membership Operators
+my_list = [17, 22, 38, 44, 57]
+print(3 in my_list)     
+print(6 not in my_list)
+# Bitwise Operators
+a = 57  
+b = 75   
+print(a & b)  
+print(a | b)  
+print(a ^ b)  
+print(~a)    
+print(a << 1) 
+print(a >> 1)
